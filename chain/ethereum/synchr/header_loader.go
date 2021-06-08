0657cc3a7f5e41331d37a7844730648b6a7703cb
jsonfelix <jsonfelix@protonmail.com>
2021-06-08 13:58:38 +0800

    add headerLoader interface realize

--- chain/ethereum/synchr/header_loader.go
+++ chain/ethereum/synchr/header_loader.go
@@ -2,16 +2,36 @@ package synchr
 
 import (
 	"context"
+	"github.com/ethereum/go-ethereum/ethclient"
 	"math/big"
 
 	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/ethclient"
 )
 
 type HeaderLoader interface {
 	HeaderByHash(ctx context.Context, hash common.Hash) (*types.Header, error)
 	HeaderByNumber(ctx context.Context, number *big.Int) (*types.Header, error)
 	SubscribeNewHead(ctx context.Context, ch chan<- *types.Header) (ethereum.Subscription, error)
+}
+
+type HeaderLoaderItem struct {
+	client *ethclient.Client
+}
+
+func NewHeaderLoader(client *ethclient.Client) *HeaderLoaderItem {
+	return &HeaderLoaderItem{client: client}
+}
+
+func (d *HeaderLoaderItem) HeaderByHash(ctx context.Context, hash common.Hash) (*types.Header, error) {
+	return d.client.HeaderByHash(ctx, hash)
+}
+
+func (d *HeaderLoaderItem) HeaderByNumber(ctx context.Context, number *big.Int) (*types.Header, error) {
+	return d.client.HeaderByNumber(ctx, number)
+}
+
+func (d *HeaderLoaderItem) SubscribeNewHead(ctx context.Context, ch chan<- *types.Header) (ethereum.Subscription, error) {
+	return d.client.SubscribeNewHead(ctx, ch)
 }
