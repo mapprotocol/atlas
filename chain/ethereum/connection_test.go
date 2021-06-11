7a0c1eb53523695910cec420215d7569afcb35c2
jsonfelix <jsonfelix@protonmail.com>
2021-06-11 13:53:30 +0800

    make connect testing for connection_test.go

---
+++ chain/ethereum/connection_test.go
@@ -0,0 +1,22 @@
+package ethereum_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/sirupsen/logrus"
+
+	"github.com/snowfork/polkadot-ethereum/relayer/chain/ethereum"
+	"github.com/snowfork/polkadot-ethereum/relayer/crypto/secp256k1"
+)
+
+func TestConnect(t *testing.T) {
+	log := logrus.NewEntry(logrus.New())
+
+	conn := ethereum.NewConnection("ws://localhost:8545", secp256k1.Bob(), log)
+	err := conn.Connect(context.Background())
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer conn.Close()
+}
