1889009bb8002cb95cd575710b9fb407e30a8017
learnforcode <2435264794@qq.com>
2021-07-08 10:33:17 +0800

    repair singer of transaction

--- core/types/transaction_signing.go
+++ core/types/transaction_signing.go
@@ -42,12 +42,8 @@ func MakeSigner(config *params.ChainConfig, blockNumber *big.Int) Signer {
 	switch {
 	case config.IsBerlin(blockNumber):
 		signer = NewEIP2930Signer(config.ChainID)
-	case config.IsEIP155(blockNumber):
-		signer = NewEIP155Signer(config.ChainID)
-	case config.IsHomestead(blockNumber):
-		signer = HomesteadSigner{}
 	default:
-		signer = FrontierSigner{}
+		signer = NewEIP155Signer(config.ChainID)
 	}
 	return signer
 }
