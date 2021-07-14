4fe2b33995a63ea4d2043144938e5467aef1daf9
neoiss <neoiss@163.com>
2021-07-14 16:22:41 +0800

    update block header validate

--- chains/validates/ethereum/header.go
+++ chains/validates/ethereum/header.go
@@ -1,37 +1,21 @@
 package ethereum
 
 import (
-	"bytes"
 	"fmt"
 	"math/big"
+	"runtime"
 	"time"
 
 	"github.com/mapprotocol/atlas/chains"
 	"github.com/mapprotocol/atlas/chains/chainsdb"
 	"github.com/mapprotocol/atlas/chains/headers/ethereum"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/common/hexutil"
-	"github.com/ethereum/go-ethereum/consensus"
-	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/log"
+	"github.com/ethereum/go-ethereum/params"
 )
 
 const (
-	EPOCH       = 300
-	extraVanity = 32                     // Fixed number of extra-data prefix bytes reserved for signer vanity
-	extraSeal   = crypto.SignatureLength // Fixed number of extra-data suffix bytes reserved for signer seal
-)
-
-var (
-	nonceAuthVote = hexutil.MustDecode("0xffffffffffffffff") // Magic nonce number to vote on adding a new signer
-	nonceDropVote = hexutil.MustDecode("0x0000000000000000") // Magic nonce number to vote on removing a signer.
-
-	uncleHash = types.CalcUncleHash(nil) // Always Keccak256(RLP([])) as uncles are meaningless outside of PoW.
-
-	diffInTurn = big.NewInt(2) // Block difficulty for in-turn signatures
-	diffNoTurn = big.NewInt(1) // Block difficulty for out-of-turn signatures
+	allowedFutureBlockTimeSeconds = int64(15)
 )
 
 type Validate struct{}
@@ -49,7 +33,6 @@ func (v *Validate) ValidateHeaderChain(chain []*ethereum.Header) (int, error) {
 			return 0, fmt.Errorf("non contiguous insert: item %d is #%d [%x..], item %d is #%d [%x..] (parent [%x..])", i-1, chain[i-1].Number,
 				parentHash.Bytes()[:4], i, chain[i].Number, hash.Bytes()[:4], chain[i].ParentHash[:4])
 		}
-		// todo bad hash
 	}
 
 	abort, results := v.VerifyHeaders(chain)
@@ -65,106 +48,119 @@ func (v *Validate) ValidateHeaderChain(chain []*ethereum.Header) (int, error) {
 }
 
 func (v *Validate) VerifyHeaders(headers []*ethereum.Header) (chan<- struct{}, <-chan error) {
-	// todo 优化？
-	abort := make(chan struct{})
-	results := make(chan error, len(headers))
+	// Spawn as many workers as allowed threads
+	workers := runtime.GOMAXPROCS(0)
+	if len(headers) < workers {
+		workers = len(headers)
+	}
 
+	// Create a task channel and spawn the verifiers
+	var (
+		inputs  = make(chan int)
+		done    = make(chan int, workers)
+		errors  = make([]error, len(headers))
+		abort   = make(chan struct{})
+		unixNow = time.Now().Unix()
+	)
+	for i := 0; i < workers; i++ {
+		go func() {
+			for index := range inputs {
+				errors[index] = v.verifyHeaderWorker(headers, index, unixNow)
+				done <- index
+			}
+		}()
+	}
+
+	errorsOut := make(chan error, len(headers))
 	go func() {
-		for i, header := range headers {
-			err := v.verifyHeader(header, headers[:i])
-
+		defer close(inputs)
+		var (
+			in, out = 0, 0
+			checked = make([]bool, len(headers))
+			inputs  = inputs
+		)
+		for {
 			select {
+			case inputs <- in:
+				if in++; in == len(headers) {
+					// Reached end of headers. Stop sending to workers.
+					inputs = nil
+				}
+			case index := <-done:
+				for checked[index] = true; checked[out]; out++ {
+					errorsOut <- errors[out]
+					if out == len(headers)-1 {
+						return
+					}
+				}
 			case <-abort:
 				return
-			case results <- err:
 			}
 		}
 	}()
-	return abort, results
+	return abort, errorsOut
 }
 
-func (v *Validate) verifyHeader(header *ethereum.Header, parents []*ethereum.Header) error {
-	if header.Number == nil {
-		return errUnknownBlock
-	}
-	number := header.Number.Uint64()
-
-	// Don't waste time checking blocks from the future
-	if header.Time > uint64(time.Now().Unix()) {
-		return consensus.ErrFutureBlock
-	}
-
-	// Checkpoint blocks need to enforce zero beneficiary
-	//checkpoint := (number % c.config.Epoch) == 0
-	// todo EPOCH
-	checkpoint := (number % EPOCH) == 0
-	//if checkpoint && header.Coinbase != (common.Address{}) {
-	//	return errInvalidCheckpointBeneficiary
-	//}
-	// Nonces must be 0x00..0 or 0xff..f, zeroes enforced on checkpoints
-	if !bytes.Equal(header.Nonce[:], nonceAuthVote) && !bytes.Equal(header.Nonce[:], nonceDropVote) {
-		return errInvalidVote
-	}
-	if checkpoint && !bytes.Equal(header.Nonce[:], nonceDropVote) {
-		return errInvalidCheckpointVote
-	}
-	// Check that the extra-data contains both the vanity and signature
-	if len(header.Extra) < extraVanity {
-		return errMissingVanity
-	}
-	if len(header.Extra) < extraVanity+extraSeal {
-		return errMissingSignature
-	}
-	// Ensure that the extra-data contains a signer list on checkpoint, but none otherwise
-	signersBytes := len(header.Extra) - extraVanity - extraSeal
-	if !checkpoint && signersBytes != 0 {
-		return errExtraSigners
-	}
-	if checkpoint && signersBytes%common.AddressLength != 0 {
-		return errInvalidCheckpointSigners
-	}
-	// Ensure that the mix digest is zero as we don't have fork protection currently
-	if header.MixDigest != (common.Hash{}) {
-		return errInvalidMixDigest
-	}
-	// Ensure that the block doesn't contain any uncles which are meaningless in PoA
-	if header.UncleHash != uncleHash {
-		return errInvalidUncleHash
-	}
-	// Ensure that the block's difficulty is meaningful (may not be correct at this point)
-	if number > 0 {
-		if header.Difficulty == nil || (header.Difficulty.Cmp(diffInTurn) != 0 && header.Difficulty.Cmp(diffNoTurn) != 0) {
-			return errInvalidDifficulty
-		}
-	}
-
-	// All basic checks passed, verify cascading fields
-	return v.verifyCascadingFields(header, parents)
-}
-
-func (v *Validate) verifyCascadingFields(header *ethereum.Header, parents []*ethereum.Header) error {
-	// The genesis block is the always valid dead-end
-	number := header.Number.Uint64()
-	if number == 0 {
-		return nil
-	}
-	// Ensure that the block's timestamp isn't too close to its parent
+func (v *Validate) verifyHeaderWorker(headers []*ethereum.Header, index int, unixNow int64) error {
 	var parent *ethereum.Header
-	if len(parents) > 0 {
-		parent = parents[len(parents)-1]
-	} else {
+	if index == 0 {
 		s, err := chainsdb.GetStoreMgr(chains.ChainTypeETH)
 		if err != nil {
 			return err
 		}
-		parent = s.ReadHeader(header.ParentHash, number-1)
+		parent = s.ReadHeader(headers[0].ParentHash, headers[0].Number.Uint64()-1)
+	} else if headers[index-1].Hash() == headers[index].ParentHash {
+		parent = headers[index-1]
+	}
+	if parent == nil {
+		return errUnknownAncestor
+	}
+	return v.verifyHeader(headers[index], parent, false, unixNow)
+}
 
+func (v *Validate) verifyHeader(header, parent *ethereum.Header, uncle bool, unixNow int64) error {
+	// Ensure that the header's extra-data section is of a reasonable size
+	if uint64(len(header.Extra)) > params.MaximumExtraDataSize {
+		return fmt.Errorf("extra-data too long: %d > %d", len(header.Extra), params.MaximumExtraDataSize)
 	}
-	if parent == nil || parent.Number.Uint64() != number-1 || parent.Hash() != header.ParentHash {
-		return consensus.ErrUnknownAncestor
+	// Verify the header's timestamp
+	if !uncle {
+		if header.Time > uint64(unixNow+allowedFutureBlockTimeSeconds) {
+			return errFutureBlock
+		}
 	}
-	if parent.Time > header.Time {
-		return errInvalidTimestamp
+	if header.Time <= parent.Time {
+		return errOlderBlockTime
+	}
+	// Verify the block's difficulty based on its timestamp and parent's difficulty
+	//expected := v.CalcDifficulty(chain, header.Time, parent)
+	//if expected.Cmp(header.Difficulty) != 0 {
+	//	return fmt.Errorf("invalid difficulty: have %v, want %v", header.Difficulty, expected)
+	//}
+
+	// Verify that the gas limit is <= 2^63-1
+	maxGas := uint64(0x7fffffffffffffff)
+	if header.GasLimit > maxGas {
+		return fmt.Errorf("invalid gasLimit: have %v, max %v", header.GasLimit, maxGas)
+	}
+	// Verify that the gasUsed is <= gasLimit
+	if header.GasUsed > header.GasLimit {
+		return fmt.Errorf("invalid gasUsed: have %d, gasLimit %d", header.GasUsed, header.GasLimit)
+	}
+
+	// Verify that the gas limit remains within allowed bounds
+	diff := int64(parent.GasLimit) - int64(header.GasLimit)
+	if diff < 0 {
+		diff *= -1
+	}
+	limit := parent.GasLimit / params.GasLimitBoundDivisor
+
+	if uint64(diff) >= limit || header.GasLimit < params.MinGasLimit {
+		return fmt.Errorf("invalid gas limit: have %d, want %d += %d", header.GasLimit, parent.GasLimit, limit)
+	}
+	// Verify that the block number is parent's +1
+	if diff := new(big.Int).Sub(header.Number, parent.Number); diff.Cmp(big.NewInt(1)) != 0 {
+		return errInvalidNumber
 	}
 
 	return nil
---
+++ test/headers_test.go
@@ -0,0 +1,110 @@
+package test
+
+import (
+	"context"
+	"log"
+	"math/big"
+	"testing"
+
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/mapprotocol/atlas/chains/chainsdb"
+	"github.com/mapprotocol/atlas/chains/headers/ethereum"
+	ve "github.com/mapprotocol/atlas/chains/validates/ethereum"
+	"github.com/mapprotocol/atlas/cmd/ethclient"
+)
+
+func dialEthConn() (*ethclient.Client, string) {
+	url := "http://127.0.0.1:8545"
+	conn, err := ethclient.Dial(url)
+	if err != nil {
+		log.Fatalf("Failed to connect to the Abeychain client: %v", err)
+	}
+	return conn, url
+}
+
+func Max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+func convertChain(e *types.Header) *ethereum.Header {
+	header := new(ethereum.Header)
+	header.ParentHash = e.ParentHash
+	header.UncleHash = e.UncleHash
+	header.Coinbase = e.Coinbase
+	header.Root = e.Root
+	header.TxHash = e.TxHash
+	header.ReceiptHash = e.ReceiptHash
+	header.GasLimit = e.GasLimit
+	header.GasUsed = e.GasUsed
+	header.Time = e.Time
+	header.MixDigest = e.MixDigest
+	header.Nonce = types.EncodeNonce(e.Nonce.Uint64())
+	header.Bloom.SetBytes(e.Bloom.Bytes())
+	if header.Difficulty = new(big.Int); e.Difficulty != nil {
+		header.Difficulty.Set(e.Difficulty)
+	}
+	if header.Number = new(big.Int); e.Number != nil {
+		header.Number.Set(e.Number)
+	}
+	if len(e.Extra) > 0 {
+		header.Extra = make([]byte, len(e.Extra))
+		copy(header.Extra, e.Extra)
+	}
+	return header
+}
+
+func getChains(startNum, endNum uint64) []*ethereum.Header {
+	conn, _ := dialEthConn()
+
+	Headers := make([]*ethereum.Header, 0, endNum-startNum+1)
+	for i := startNum; i <= endNum; i++ {
+		Header, _ := conn.HeaderByNumber(context.Background(), big.NewInt(int64(i)))
+		if Header != nil {
+			Headers = append(Headers, convertChain(Header))
+		}
+	}
+	return Headers
+}
+
+func TestHeader_ValidateHeaderChain(t *testing.T) {
+
+	type args struct {
+		chain []*ethereum.Header
+	}
+	tests := []struct {
+		name    string
+		args    args
+		before  func()
+		want    int
+		wantErr bool
+	}{
+		{
+			name: "t-1",
+			args: args{
+				chain: getChains(0, 300),
+			},
+			before: func() {
+				chainsdb.NewStoreDb(nil, 10, 2)
+			},
+			want:    0,
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.before()
+			eh := &ve.Validate{}
+			got, err := eh.ValidateHeaderChain(tt.args.chain)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ValidateHeaderChain() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("ValidateHeaderChain() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
