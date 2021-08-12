4b40dd2a3e6a22d32d2f9e998492be466f2f754c
zhangwei <09552700>
2021-08-12 14:20:37 +0800

    commit  header config 1. delete   ChainTypeETH_ropsten rawdb.ChainType = 1004 2. delete relayer_cli  relayer_mock eth_client

--- chains/constants.go
+++ chains/constants.go
@@ -5,11 +5,10 @@ import (
 )
 
 const (
-	ChainTypeMAP         rawdb.ChainType = 1000
-	ChainTypeETH         rawdb.ChainType = 1001
-	ChainTypeETH_test    rawdb.ChainType = 1002
-	ChainTypeETH_dev     rawdb.ChainType = 1003
-	ChainTypeETH_ropsten rawdb.ChainType = 1004 // start 800
+	ChainTypeMAP      rawdb.ChainType = 1000
+	ChainTypeETH      rawdb.ChainType = 1001
+	ChainTypeETH_test rawdb.ChainType = 1002 // start 800
+	ChainTypeETH_dev  rawdb.ChainType = 1003
 )
 
 var ChainTypeList = []rawdb.ChainType{
@@ -17,7 +16,6 @@ var ChainTypeList = []rawdb.ChainType{
 	ChainTypeETH,
 	ChainTypeETH_test,
 	ChainTypeETH_dev,
-	ChainTypeETH_ropsten,
 }
 
 func IsSupportedChain(chain rawdb.ChainType) bool {
