e68dab698662b575e8d69929ccfe67176d4c2903
mapdev33 <>
2021-08-13 17:36:44 +0800

    rename package name for atlas

--- atlas/downloader/metrics.go
+++ atlas/downloader/metrics.go
@@ -23,23 +23,23 @@ import (
 )
 
 var (
-	headerInMeter      = metrics.NewRegisteredMeter("eth/downloader/headers/in", nil)
-	headerReqTimer     = metrics.NewRegisteredTimer("eth/downloader/headers/req", nil)
-	headerDropMeter    = metrics.NewRegisteredMeter("eth/downloader/headers/drop", nil)
-	headerTimeoutMeter = metrics.NewRegisteredMeter("eth/downloader/headers/timeout", nil)
+	headerInMeter      = metrics.NewRegisteredMeter("atlas/downloader/headers/in", nil)
+	headerReqTimer     = metrics.NewRegisteredTimer("atlas/downloader/headers/req", nil)
+	headerDropMeter    = metrics.NewRegisteredMeter("atlas/downloader/headers/drop", nil)
+	headerTimeoutMeter = metrics.NewRegisteredMeter("atlas/downloader/headers/timeout", nil)
 
-	bodyInMeter      = metrics.NewRegisteredMeter("eth/downloader/bodies/in", nil)
-	bodyReqTimer     = metrics.NewRegisteredTimer("eth/downloader/bodies/req", nil)
-	bodyDropMeter    = metrics.NewRegisteredMeter("eth/downloader/bodies/drop", nil)
-	bodyTimeoutMeter = metrics.NewRegisteredMeter("eth/downloader/bodies/timeout", nil)
+	bodyInMeter      = metrics.NewRegisteredMeter("atlas/downloader/bodies/in", nil)
+	bodyReqTimer     = metrics.NewRegisteredTimer("atlas/downloader/bodies/req", nil)
+	bodyDropMeter    = metrics.NewRegisteredMeter("atlas/downloader/bodies/drop", nil)
+	bodyTimeoutMeter = metrics.NewRegisteredMeter("atlas/downloader/bodies/timeout", nil)
 
-	receiptInMeter      = metrics.NewRegisteredMeter("eth/downloader/receipts/in", nil)
-	receiptReqTimer     = metrics.NewRegisteredTimer("eth/downloader/receipts/req", nil)
-	receiptDropMeter    = metrics.NewRegisteredMeter("eth/downloader/receipts/drop", nil)
-	receiptTimeoutMeter = metrics.NewRegisteredMeter("eth/downloader/receipts/timeout", nil)
+	receiptInMeter      = metrics.NewRegisteredMeter("atlas/downloader/receipts/in", nil)
+	receiptReqTimer     = metrics.NewRegisteredTimer("atlas/downloader/receipts/req", nil)
+	receiptDropMeter    = metrics.NewRegisteredMeter("atlas/downloader/receipts/drop", nil)
+	receiptTimeoutMeter = metrics.NewRegisteredMeter("atlas/downloader/receipts/timeout", nil)
 
-	stateInMeter   = metrics.NewRegisteredMeter("eth/downloader/states/in", nil)
-	stateDropMeter = metrics.NewRegisteredMeter("eth/downloader/states/drop", nil)
+	stateInMeter   = metrics.NewRegisteredMeter("atlas/downloader/states/in", nil)
+	stateDropMeter = metrics.NewRegisteredMeter("atlas/downloader/states/drop", nil)
 
-	throttleCounter = metrics.NewRegisteredCounter("eth/downloader/throttle", nil)
+	throttleCounter = metrics.NewRegisteredCounter("atlas/downloader/throttle", nil)
 )
