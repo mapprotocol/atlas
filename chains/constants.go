64bfc4d4bcb3870cbf168410370ec724424cfdcb
neoiss <neoiss@163.com>
2021-09-29 17:45:13 +0800

    modified ChainTypeMAP to 177

--- chains/constants.go
+++ chains/constants.go
@@ -4,10 +4,11 @@ import (
 	"math/big"
 
 	"github.com/mapprotocol/atlas/core/rawdb"
+	params2 "github.com/mapprotocol/atlas/params"
 )
 
 const (
-	ChainTypeMAP     rawdb.ChainType = 211
+	ChainTypeMAP     rawdb.ChainType = rawdb.ChainType(params2.MainNetChainID)
 	ChainTypeETH     rawdb.ChainType = 1
 	ChainTypeETHTest rawdb.ChainType = 3 // start 800
 	ChainTypeETHDev  rawdb.ChainType = 10
