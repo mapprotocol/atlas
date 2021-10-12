284018e5667d35001f7b0afff9f0d8e306bac39f
learnforcode <2435264793@qq.com>
2021-10-12 11:42:01 +0800

    transaction can submit to chain

--- core/chain/tx_list.go
+++ core/chain/tx_list.go
@@ -18,7 +18,6 @@ package chain
 
 import (
 	"container/heap"
-	"fmt"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/math"
 	"github.com/ethereum/go-ethereum/log"
@@ -1379,7 +1378,6 @@ func (l *txPricedList) Removed(count int) {
 	if l.all.nilCurrencyTxCurrCount > uint64(l.Len()) {
 		l.all.nilCurrencyTxCurrCount = uint64(l.Len())
 	}
-	fmt.Println("l.all.nilCurrencyTxCurrCount", l.all.nilCurrencyTxCurrCount)
 	// Seems we've reached a critical number of stale transactions, reheap
 	reheapNilCurrencyHeap := make(priceHeap, 0, l.all.nilCurrencyTxCurrCount)
 
