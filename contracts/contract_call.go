863d06cf6843d9376d2118b1ea674cb381dad599
zhangwei <1733773547@qq.com>
2021-11-04 13:44:37 +0800

    abi decodeResult am.abi.Unpack->am.abi.UnpackIntoInterface

--- contracts/contract_call.go
+++ contracts/contract_call.go
@@ -45,11 +45,8 @@ func (am Method) decodeResult(result interface{}, output []byte) error {
 	if result == nil {
 		return nil
 	}
-
 	// adapter new pattern
-	results, err := am.abi.Unpack(am.method, output)
-	// fetch first data
-	result = results[0]
+	err := am.abi.UnpackIntoInterface(result, am.method, output)
 	return err
 }
 
