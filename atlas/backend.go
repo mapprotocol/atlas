d5b82497e5c7c4b83da25cb78e1ce08861963b84
mapdev33 <>
2021-11-10 10:17:27 +0800

    update namespace for OpenDatabaseWithFreezer

--- atlas/backend.go
+++ atlas/backend.go
@@ -132,7 +132,7 @@ func New(stack *node.Node, config *ethconfig.Config) (*Ethereum, error) {
 	log.Info("Allocated trie memory caches", "clean", common.StorageSize(config.TrieCleanCache)*1024*1024, "dirty", common.StorageSize(config.TrieDirtyCache)*1024*1024)
 
 	// Assemble the Ethereum object
-	chainDb, err := stack.OpenDatabaseWithFreezer("chaindata", config.DatabaseCache, config.DatabaseHandles, config.DatabaseFreezer, "eth/db/chaindata/", false)
+	chainDb, err := stack.OpenDatabaseWithFreezer("chaindata", config.DatabaseCache, config.DatabaseHandles, config.DatabaseFreezer, "atlas/db/chaindata/", false)
 	if err != nil {
 		return nil, err
 	}
