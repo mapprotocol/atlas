3880a2fba931f44755003782f2d3dc3c07dec323
neoiss <neoiss@163.com>
2021-11-16 16:17:37 +0800

    modified file

--- consensus/istanbul/logger.go
+++ consensus/istanbul/logger.go
@@ -1,18 +1,18 @@
-// Copyright 2017 The celo Authors
-// This file is part of the celo library.
-//
-// The celo library is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Lesser General Public License as published by
+// Copyright 2021 MAP Protocol Authors.
+// This file is part of MAP Protocol.
+
+// MAP Protocol is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-//
-// The celo library is distributed in the hope that it will be useful,
+
+// MAP Protocol is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU Lesser General Public License for more details.
-//
-// You should have received a copy of the GNU Lesser General Public License
-// along with the celo library. If not, see <http://www.gnu.org/licenses/>.
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with MAP Protocol.  If not, see <http://www.gnu.org/licenses/>.
 
 package istanbul
 //
