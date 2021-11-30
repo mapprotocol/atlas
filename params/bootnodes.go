fdc77504a18279fd79f679b7e3166fe4259324ae
neoiss <neoiss@163.com>
2021-11-30 13:57:54 +0800

    del testnet boot nodes

--- params/bootnodes.go
+++ params/bootnodes.go
@@ -13,12 +13,7 @@ var MainnetBootnodes = []string{
 
 // TestnetBootnodes are the enode URLs of the P2P bootstrap nodes running on the
 // test network.
-var TestnetBootnodes = []string{
-	"enode://1457e5fc1cc9a1584aacbc860922563d049a2efb9aa0fc73089c85769f1e39e53c3419984af0af75cd29177af1b62e90ac86912562c05583b6cd158fd532f0d5@20.205.187.105:36201",
-	"enode://ec9e7dee840333b72addfb6d1b76020bd9a72f0cb113dbd18ff3f51d70e03d80b23c63e102fbeb4ecad36da03d2d4fb6afaa6178849d499ba653d2c55013497d@20.205.187.105:36202",
-	"enode://b02d993aa6f81012e532f9b494d1ab3036cabbbd6401a40fb0c3133ed3997887e41f800de8e9da9f3a94665270a1b14c7346d9c13332397bca953bf65538e715@20.205.189.217:36203",
-	"enode://dcbe8b57c5c265668a33e4f41214b1161860602d22d710a713e4cc2005a3cf2bc417be10a8a1d251ffe7e55e566319d368826103dba159d734b3ee4fa7c02c76@20.205.189.217:36204",
-}
+var TestnetBootnodes = []string{}
 
 const dnsPrefix = "enrtree://AKA3AM6LPBYEUDMVNU3BSVQJ5AD45Y7YPOHJLEF6W26QOE4VTUDPE@"
 
