2d92455a31fd2e95599cad412e675b07284c0884
neoiss <neoiss@163.com>
2022-01-27 18:09:58 +0800

    update DefaultTxPoolConfig

--- core/chain/tx_pool.go
+++ core/chain/tx_pool.go
@@ -183,10 +183,10 @@ var DefaultTxPoolConfig = TxPoolConfig{
 	PriceLimit: 1,
 	PriceBump:  10,
 
-	AccountSlots: 16,
-	GlobalSlots:  4096 + 1024, // urgent + floating queue capacity with 4:1 ratio
-	AccountQueue: 64,
-	GlobalQueue:  1024,
+	AccountSlots: 160,
+	GlobalSlots:  40960 + 10240, // urgent + floating queue capacity with 4:1 ratio
+	AccountQueue: 2000,
+	GlobalQueue:  10240,
 
 	Lifetime: 3 * time.Hour,
 }
