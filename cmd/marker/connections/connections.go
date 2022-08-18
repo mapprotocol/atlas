8d109b80e977cc2f1f3a010fc2b597097ad15a08
neoiss <neoiss@163.com>
2022-08-18 20:09:36 +0800

    marker supported multiple ways to specify rpc address

--- cmd/marker/connections/connections.go
+++ cmd/marker/connections/connections.go
@@ -1,7 +1,11 @@
 package connections
 
 import (
+	"context"
 	"fmt"
+	"net"
+	"strings"
+
 	"gopkg.in/urfave/cli.v1"
 
 	"github.com/ethereum/go-ethereum/ethclient"
@@ -10,15 +14,48 @@ import (
 	"github.com/mapprotocol/atlas/cmd/marker/config"
 )
 
+const (
+	httpScheme  = "http"
+	httpsScheme = "https"
+)
+
 func DialConn(ctx *cli.Context, config *config.Config) (*ethclient.Client, string) {
-	logger := log.New("func", "dialConn")
-	ip := config.Ip     //utils.RPCListenAddrFlag.Name)
-	port := config.Port //utils.RPCPortFlag.Name)
-	url := fmt.Sprintf("http://%s", fmt.Sprintf("%s:%d", ip, port))
-	//url := "https://poc2-rpc.maplabs.io"
+	var (
+		url    string
+		host   string
+		scheme string
+	)
+	ip := config.Ip
+	port := config.Port
+	parts := strings.Split(ip, "://")
+	addr := parts[len(parts)-1]
+
+	if len(parts) > 1 {
+		scheme = parts[0]
+	}
+	if port != 0 {
+		host = fmt.Sprintf("%s:%d", addr, port)
+	} else {
+		host = addr
+	}
+	if net.ParseIP(ip) != nil {
+		if scheme == "" {
+			scheme = httpScheme
+		}
+	} else {
+		if scheme == "" {
+			scheme = httpsScheme
+		}
+	}
+	url = fmt.Sprintf("%s://%s", scheme, host)
 	conn, err := ethclient.Dial(url)
 	if err != nil {
-		logger.Error("Failed to connect to the Atlaschain client: %v", err)
+		panic(fmt.Sprintf("Failed to connect to the map chain url: %v", err))
+	}
+
+	_, err = conn.ChainID(context.Background())
+	if err != nil {
+		panic(err)
 	}
 	return conn, url
 }
